f5f67b5e3735d191dae5c4838486741faefb4e0a
Prakhar Saraswat <prakhar.saraswat@geminisolutions.com>
2023-08-16 22:19:58 +0530

    Added SCSS to about component

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,6 +1,5 @@
 import multer from "multer";
 import { GridFsStorage } from "multer-gridfs-storage";
-
 import dotenv from "dotenv"
 
 dotenv.config();
@@ -13,7 +12,6 @@ const storage = new GridFsStorage({
 
     if (match.indexOf(file.memeType) === -1)
       return `${Date.now()}-blog-${file.originalname}`;
-
     return {
       bucketName: "photos",
       filename: `${Date.now()}-blog-${file.originalname}`,
